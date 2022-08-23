c6ec12898b9a6a2cf3b0359d14caf72dbeb33f1b
Vladimir <vladimirkristall@yandex.ru>
2022-08-23 22:33:11 +0300

    Add Task42

---
+++ Seminar6/Task42/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+void ReverseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+}
+
+Console.WriteLine("Введите десятичное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int size = (int)Math.Log2(number) + 1;
+int[] array = new int[size];
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = number % 2;
+    number = number / 2;
+}
+ReverseArray(array);
+Console.WriteLine(string.Join("", array));
+
+// второй вариант с помощью рекурсии
+
+// Console.WriteLine("Введите колличество символов");
+// int number = int.Parse(Console.ReadLine()!);
+
+// GetBinaryView(number);
+
+// void GetBinaryView(int N)
+// {
+//     if (N <= 0) return;
+//     GetBinaryView(N / 2);
+//     Console.Write(N % 2);
+// }

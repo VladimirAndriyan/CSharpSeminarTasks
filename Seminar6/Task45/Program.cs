c6adcae342a47437d4bc19c519b36d828555a2ff
Vladimir <vladimirkristall@yandex.ru>
2022-08-23 22:59:35 +0300

    Add Task45

---
+++ Seminar6/Task45/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+
+int[] array = {1,2,5,7,8};
+int[] newarray = new int[array.Length];
+for(int i = 0; i < array.Length; i++)
+{
+   newarray[i] = array[i];
+}
+array[4] = 10;
+Console.WriteLine(string.Join(" ", array));
+Console.WriteLine(string.Join(" ", newarray));

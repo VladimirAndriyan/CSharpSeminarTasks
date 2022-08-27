4ea29872b77f2a273fc9e98f8b2bb6145299d6cb
Vladimir <vladimirkristall@yandex.ru>
2022-08-27 23:31:36 +0300

    Add Task51

---
+++ Seminar7/Task51/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д.Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+// Доп. условие для 51 задачи: сделать суммирование в один цикл. Матрица может быть прямоугольный
+
+
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = -10, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите число строк m "); 
+int m = Convert.ToInt32(Console.ReadLine());//число строк
+Console.Write("Введите число стобцов n ");
+int n = Convert.ToInt32(Console.ReadLine());//число столбцов
+int[,] matrix = FillMatrix(m, n, 0, 10);
+PrintMatrix(matrix);
+int sum = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+           if(i == j)
+           {
+                sum += matrix[i, j];
+           }
+        }
+    }
+Console.WriteLine($"Сумма элементов главной диагонали {sum}");
